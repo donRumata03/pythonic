b24063eea4c6d9d4c933d68365fafd4d4c047e87
donRumata <donrumata03@gmail.com>
2020-07-25 19:13:30 +0300

    use more of pch.h

--- timing/delayed_launcher.h
+++ timing/delayed_launcher.h
@@ -1,9 +1,13 @@
 #pragma once
 
+/*
 #include <functional>
 #include <chrono>
 #include <future>
 #include <cstdio>
+*/
+
+#include "pch.h"
 
 template <class callable, class... arguments>
 class delayed_launcher
