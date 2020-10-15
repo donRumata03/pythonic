e151a4d1c74f3d6ceb3ad4a6f9c85225398723ba
donRumata <donrumata03@gmail.com>
2020-10-15 17:21:33 +0300

    toupper in linux, threading

---
+++ tests/threading_tests/thread_distribution_test.h
@@ -0,0 +1,27 @@
+//
+// Created by Vova on 15.10.2020.
+//
+
+#pragma once
+
+#include <pythonic.h>
+
+inline void test_thread_distribution ()
+{
+	//                    tasks | threads
+	std::vector<std::pair<size_t, size_t>> examples = {
+			{ 0, 0 },
+			{ 1, 1 },
+			{ 1, 1 },
+			{ 1, 1 },
+
+
+	};
+
+	std::cout << "Testing thread distributing" << std::endl;
+
+	for (auto& example : examples) {
+		std::vector<std::pair<size_t, size_t>> ranges = distribute_task_ranges(example.first, example.second);
+		std::cout << example << "\t\t\t : \t\t\t" << ranges << std::endl;
+	}
+}
