6efa947604fae7ffed724759ca6c02ffcf3775bb
donRumata <donrumata03@gmail.com>
2020-10-18 00:11:24 +0300

    develop thread_pool interface

--- threading/static_thread_pool.cpp
+++ threading/static_thread_pool.cpp
@@ -2,14 +2,16 @@
 // Created by Vova on 17.10.2020.
 //
 
+
 #include "static_thread_pool.h"
 
-void thread_wrapper(size_t thread_index, const std::function<void(size_t)>& function_to_launch_at_each_iteration) {
-	while (true) {
-		//TODO
-		function_to_launch_at_each_iteration(thread_index);
-	}
-}
+
+/**
+ * Public methods:
+ */
+
+void thread_wrapper(size_t thread_index, const std::function<void(size_t)>& function_to_launch_at_each_iteration);
+
 
 static_thread_pool::static_thread_pool (size_t thread_number, const std::function<void(size_t)>& function_to_launch_at_each_iteration)
 {
@@ -17,3 +19,36 @@ static_thread_pool::static_thread_pool (size_t thread_number, const std::functio
 		m_threads.emplace_back(thread_wrapper, thread_index, function_to_launch_at_each_iteration);
 	}
 }
+
+
+void static_thread_pool::compute ()
+{
+	inform_thread_ending_state(true);
+
+	wait_for_threads_ready();
+}
+
+void static_thread_pool::join ()
+{
+	inform_thread_ending_state(false);
+
+	for(auto& thread : m_threads) thread.join();
+}
+
+
+void thread_wrapper(size_t thread_index, const std::function<void(size_t)>& function_to_launch_at_each_iteration) {
+	while (true) {
+		// TODO
+		function_to_launch_at_each_iteration(thread_index);
+	}
+}
+
+
+
+
+void static_thread_pool::inform_thread_ending_state (bool continue_or_not)
+{
+
+}
+
+
--- threading/static_thread_pool.h
+++ threading/static_thread_pool.h
@@ -5,10 +5,25 @@
 #pragma once
 
 #include "pythonic_pch.h"
+#include "other/typedefs.h"
 
 class static_thread_pool
 {
 	std::vector<std::thread> m_threads;
+	// std::atomic<bool> threads_are_working = false; <- todo!
+
+	std::mutex running_cv_protector;
+	bool thread_ending_state_ready = false;
+	bool threads_should_be_run = false;
+	std::condition_variable running_cv;
+
+	std::vector<bool> threads_ready;    // Updated by threads after ending,
+										// read by main thread to set true to all_threads_ready,
+										// is reset by main thread after ending
+	std::atomic<lint> max_all_threads_ready_run = -1;   // All the threads have finished work for all the indexes <= max_all_threads_ready_run
+														// Set by main thread,
+														// All the threads are waiting for all the others to be ready.
+
 
 public:
 	explicit static_thread_pool(size_t thread_number, const std::function<void(size_t)>& function_to_launch_at_each_iteration);
@@ -22,9 +37,11 @@ public:
 	////////////////////////////////////////////////////////////////////////
 
 	void join();
-	void launch_tasks();
+	void compute();
 
-
+private:
+	void inform_thread_ending_state(bool continue_or_not);
+	void wait_for_threads_ready();
 };
 
 
