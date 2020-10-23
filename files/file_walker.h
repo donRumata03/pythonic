944b9456c1edc7981c0529098566005d7fbd8975
donRumata03 <donrumata03@gmail.com>
2020-10-23 11:45:54 +0300

    fix multithreading

--- files/file_walker.h
+++ files/file_walker.h
@@ -11,8 +11,16 @@
 
 #include <encoding/encoder.h>
 
+#ifdef PYTHONIC_IS_LINUX
+#include "files/platform/linux/file_walker_linux.h"
+#endif
+
+#ifdef PYTHONIC_IS_WINDOWS
+#include "files/platform/windows/file_walker_windows.h"
+#endif
 
 
+/*
 std::vector<std::string> lsdir(const std::string& path); /// path is assumed to be encoded in utf-8; result is also utf-8
 
 std::vector<std::string> recursive_lsdir(const std::string& path); /// path is assumed to be encoded in utf-8; result is also utf-8
@@ -22,3 +30,5 @@ inline size_t fs_file_size(const std::string& path) { return fs_file_size(recode
 
 size_t fs_folder_total_size(const std::wstring& path);
 inline size_t fs_folder_total_size(const std::string& path) { return fs_folder_total_size(recode::from_utf8_to_utf16(path)); }
+
+*/
