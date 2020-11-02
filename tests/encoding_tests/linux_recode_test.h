de1ba25a13b1f3573c95032352e23c80de467343
donRumata03 <donrumata03@gmail.com>
2020-11-02 21:24:45 +0300

    make tests compile for linux after restricting read_file without explicit template

--- tests/encoding_tests/linux_recode_test.h
+++ tests/encoding_tests/linux_recode_test.h
@@ -9,7 +9,7 @@
 
 inline void test_reading_cp1251_file() {
     std::string filename = (pythonic_base_dir / "tests" / "encoding_tests" / "test_files" / "Привет-cp1251.txt").string();
-    auto file_contents = *read_file(filename);
+    auto file_contents = *read_file<given_filename_encoding::utf8>(filename);
 
     std::cout << file_contents << std::endl;
     std::cout << recode::to_utf8(file_contents) << std::endl;
