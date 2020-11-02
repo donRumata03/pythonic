8c5e2e4dcff8bee4dc56960242545dc1837463d8
donRumata <donrumata03@gmail.com>
2020-11-02 21:20:10 +0300

    file operations with filename represented as std::filesystem::path in linux

--- files/platform/linux/file_reader_linux.cpp
+++ files/platform/linux/file_reader_linux.cpp
@@ -2,5 +2,19 @@
 // Created by Vova on 14.10.2020.
 //
 
+#include "file_reader_linux.h"
+
+
+std::optional<std::string> read_file(const std::filesystem::path& filename) {
+	return read_file<given_filename_encoding::utf8>(filename.string());
+}
+
+void write_file(const std::string& data, const std::filesystem::path& filename) {
+	write_file<given_filename_encoding::utf8>(data, filename.string());
+}
+
+void file_size_in_bytes(const std::filesystem::path& filename) {
+	file_size_in_bytes<given_filename_encoding::utf8>(filename.string());
+}
 
 
