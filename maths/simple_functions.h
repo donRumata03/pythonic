91c15a76cc86087b9cb88a111f597a6b6d165da6
donRumata <donrumata03@gmail.com>
2021-02-09 15:29:55 +0300

    fix almost equal function for zeros

--- maths/simple_functions.h
+++ maths/simple_functions.h
@@ -28,10 +28,12 @@ inline T cube(const T& val)
 }
 
 template <class Float, std::enable_if_t<std::is_floating_point_v<Float>, void*> nothing = nullptr>
-bool almost_equal(Float float1, Float float2, Float eps = 1e-10) {
+bool almost_equal(Float float1, Float float2, Float relative_eps = 1e-10, Float absolute_eps = 1e-15) {
 	if (float1 == float2) return true;
 	if ((float1 + float2) == 0) return false;
-	return std::abs((float1 - float2) / (float1 + float2)) < eps;
+	if(float1 == 0) return std::abs(float2) < absolute_eps;
+	if(float2 == 0) return std::abs(float1) < absolute_eps;
+	return std::abs((float1 - float2) / (float1 + float2)) < relative_eps;
 }
 
 inline constexpr double pi = 3.1415926535897932384626433832795028841971693993751058209749;
